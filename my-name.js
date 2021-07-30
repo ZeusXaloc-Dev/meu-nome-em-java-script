2667ee816a31fdd9f9cc0916599cf7e2a51c6ef2
Zeus Xaloc Dev <85583693+ZeusXaloc-Dev@users.noreply.github.com>
2021-07-30 00:09:27 -0400

    Create my-name.js

---
+++ my-name.js
@@ -0,0 +1,11 @@
+var nome = 'Zeus Xaloc'; // Ei Variavel Nome; 
+
+function RetornarNome(){
+  nome = 'Luan Xaloc'; //retribuição de valor;
+   return nome // Não atribuir este valor à lugar algum;
+}
+
+RetornarNome();
+  console.log(nome); 
+  
+  // Meu Estudo em Js Iniciou turma!
